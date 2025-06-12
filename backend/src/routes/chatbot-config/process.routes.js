3314d68b00d38bf84ec30889a06180f88d54a582
alexfonfdz <fofapersonal@gmail.com>
2025-06-11 20:05:02 -0700

    Errorcito en permisos de API

--- backend/src/routes/chatbot-config/process.routes.js
+++ backend/src/routes/chatbot-config/process.routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { getAllProcesses, getProcess, createProcessController, updateProcessController, toggleProcessActiveController } from '../../controllers/chatbot-config/process.controller.js';
-import { authenticateToken, isPAT } from '../../middlewares/auth.middleware.js';
+import { authenticateToken, isAny, isPAT } from '../../middlewares/auth.middleware.js';
 
 const router = Router();
 
-router.get('/', authenticateToken, isPAT, getAllProcesses);
+router.get('/', authenticateToken, isAny, getAllProcesses);
 router.get('/:id', authenticateToken, isPAT, getProcess);
 router.post('/', authenticateToken, isPAT, createProcessController);
 router.put('/:id', authenticateToken, isPAT, updateProcessController);
